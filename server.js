caa48fc76a75e900b624ad066da848f740807a9d
thandosim <sim24001@byui.edu>
2025-07-02 13:48:23 +0200

    set up connecion to database

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 const express = require('express');
+
+const mongodb = require('./data/database');
 const app = express();
 
 const port = process.env.PORT || 3000;
 
 app.use('/', require('./routes'));
 
-app.listen(port, () => {
-  console.log(`Server is running on http://localhost:${port}`);
+
+
+mongodb.initDb((err) => {
+    if(err) {
+        console.log(err);
+    }
+    else {
+        app.listen(port, () => {
+            console.log(`Database is listening and running on http://localhost:${port}`);
+        });
+    }
 });
