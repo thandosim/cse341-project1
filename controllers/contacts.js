e62d5e7363dd9004937894fd259399b4b8f98b14
thandosim <sim24001@byui.edu>
2025-07-03 14:07:24 +0200

    finished making contacts api work with getsingle and getall

---
+++ controllers/contacts.js
@@ -0,0 +1,25 @@
+const mongodb = require('../data/database');
+const ObjectId = require('mongodb').ObjectId;
+
+const getAll = async (req, res) => {
+    const result = await mongodb.getDatabase().db('project1').collection('contacts').find();
+    result.toArray().then((contacts) => {
+        res.setHeader('content-type', 'application/json');
+        res.status(200).json(contacts);
+    });
+};
+
+
+const getSingle = async (req, res) => {
+    const contactId = new ObjectId(req.params.id);
+    const result = await mongodb.getDatabase().db('project1').collection('contacts').find({_id: contactId});
+    result.toArray().then((contacts) => {
+        res.setHeader('content-type', 'application/json');
+        res.status(200).json(contacts[0]);
+    });
+};
+
+module.exports = {
+    getAll,
+    getSingle
+};
