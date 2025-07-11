21c110301af85cfa1611429deab31b1aca8f5178
thandosim <sim24001@byui.edu>
2025-07-11 11:54:25 +0200

    fix: update host configuration in Swagger setup and clean up test.rest URLs
    to make it work in local and in production

--- swagger.js
+++ swagger.js
@@ -1,13 +1,14 @@
 
 const swaggerAutogen = require('swagger-autogen')();
+const host = process.env.HOST || 'localhost:3000'; // to make this work in both local and production environments
 
 const doc = {
   info: {
     title: 'Contacts API',
     description: 'list of contacts with CRUD operations'
   },
-  host: 'localhost:3000',
-  schemes: ['http']
+  host: host,
+  schemes: ['http', 'https'], // 'http' for local development, 'https' for production
 };
 
 const outputFile = './swagger-output.json';
